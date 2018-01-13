5e64230c12d7a60ff4651c5c32a61edff3f2df00
Ali Kemal Civelek <ali@sirketing.com>
2018-01-14 00:19:12 +0300

    Added custom api guard.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,29 +2,33 @@
 
 namespace App\Providers;
 
+use App\Services\Auth\ApiGuard;
+
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
-class AuthServiceProvider extends ServiceProvider
-{
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+use Auth;
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+class AuthServiceProvider extends ServiceProvider {
+	/**
+	 * The policy mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $policies = [
+		'App\Model' => 'App\Policies\ModelPolicy',
+	];
 
-        //
-    }
+	/**
+	 * Register any authentication / authorization services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		$this->registerPolicies();
+
+		Auth::extend('api', function($app, $name, array $config) {
+			return new ApiGuard(Auth::createUserProvider($config['provider']), $app['request']);
+		});
+	}
 }
